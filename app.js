7105e69d8b0653ea24c6d79671680f91b51a3831
Marcin Szabłowski <marcin.szablowski@op.pl>
2020-04-19 17:34:26 +0200

    Add overtime functionality

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   let countdown;
+  let overtime;
   let audio = new Audio("./audio/bell.mp3")
+  const display = document.querySelector(".display");
   const timeDisplay = document.querySelector(".display-time");
   const hourDisplay = document.querySelector(".display-hour");
   const buttons = document.querySelectorAll("[data-time]");
@@ -10,64 +12,107 @@ document.addEventListener('DOMContentLoaded', function () {
     // Clear any existing timers
     timeDisplay.style.color = "black";
     clearInterval(countdown);
+    clearInterval(overtime);
 
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
     displayEndHour(then);
 
+
     countdown = setInterval(() => {
       // Time left in seconds
       const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-      // Check if it should stop
-      if (secondsLeft < 0) {
+      // Check if it should stop and go over
+      if (secondsLeft <= 0) {
+        timeDisplay.style.color = "white";
+        hourDisplay.style.color = "white";
+        display.style.backgroundColor = "#e60000";
+      }
+
+      if (secondsLeft <= 0) {
         clearInterval(countdown);
+        displayTimeOver();
         return;
       }
 
       // Display
       displayTimeLeft(secondsLeft);
 
-      if(secondsLeft < 10) {
+      if (secondsLeft < 10) {
         timeDisplay.style.color = "red";
       }
 
-      if(secondsLeft === 0) {
+      if (secondsLeft === 0) {
         audio.play();
       }
     }, 1000);
   }
 
+
+  function displayTimeOver() {
+
+    let minutesOver = 0;
+    let secondsOver = 0;
+
+    timeDisplay.textContent = "0:00";
+
+    overtime = setInterval(() => {
+      secondsOver++;
+      if (secondsOver === 60) {
+        secondsOver = 0;
+        minutesOver++;
+      }
+      console.log({ minutesOver, secondsOver });
+      let display = `-${minutesOver}:${((secondsOver < 10) ? "0" : "")}${secondsOver}`
+      timeDisplay.textContent = display;
+    }, 1000)
+
+  }
+
   function displayTimeLeft(seconds) {
-    
+
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    console.log({minutes, remainderSeconds});
-    const display = `${minutes}:${(remainderSeconds < 10 ? "0" : "")}${remainderSeconds}`;
+
+    console.log({ minutes, remainderSeconds });
+    let display = `${minutes}:${((remainderSeconds < 10 && remainderSeconds >= 0) ? "0" : "")}${remainderSeconds}`;
     document.title = display;
     timeDisplay.textContent = display;
+
+    if (remainderSeconds <= 0) {
+      timeDisplay.style.color = "white";
+      hourDisplay.style.color = "white";
+      // display.style.backgroundColor = "#e60000";
+    }
   }
 
   function displayEndHour(timestamp) {
     const end = new Date(timestamp);
     const hour = end.getHours();
     const minutes = end.getMinutes();
-    hourDisplay.textContent = `Break ends at ${hour}:${minutes < 10 ? "0" : ""}${minutes}`;
+    hourDisplay.textContent = `Koniec przerwy ${hour}:${minutes < 10 ? "0" : ""}${minutes}`;
   }
 
-function startTimer() {
-  const seconds = parseInt(this.dataset.time);
-  time(seconds);
-}
+  function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    time(seconds);
+    display.style.backgroundColor = "white";
+    timeDisplay.style.color = "black";
+    hourDisplay.style.color = "black";
+  }
 
-buttons.forEach(button => button.addEventListener("click", startTimer));
+  buttons.forEach(button => button.addEventListener("click", startTimer));
 
-document.customForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  const minutes = this.minutes.value;
-  time(minutes * 60);
-  this.reset();
-})
+  document.customForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const minutes = this.minutes.value;
+    time(minutes * 60);
+    this.reset();
+    display.style.backgroundColor = "white";
+    timeDisplay.style.color = "black";
+    hourDisplay.style.color = "black";
+  })
 
 });
